d03792354ed268cd98df0a7fafe380f0a0b0416b
SanuelDeVries <samuelad2019@outlook.com>
2025-04-17 16:32:22 -0400

    Continuation de la fonction de réservation

--- fonction_reservation.py
+++ fonction_reservation.py
@@ -1,4 +1,13 @@
+import csv
 def reserver():
+    with open("reservations.csv", "r") as fichier_reservations:
+        reservations = csv.reader(fichier_reservations)
+        next(reservations)
+        reservations = list(reservations)
+        try:
+            id_reservation = reservations[-1][0]+1
+        except IndexError:
+            id_reservation = 1
     nom_utilisateur = input("À quel nom est la réservation?")
     print("Choisissez une salle parmie les suivant.")
     with open("salles.txt", "r") as salles:
@@ -12,5 +21,12 @@ def reserver():
         for nb_ligne, ligne in enumerate(liste_salles):
             if nb_salle == nb_ligne:
                 nom_salle = ligne.split("\t")[0]
-    print(nom_salle)
+    date = input("Pour quelle date souhaitez-vous faire la réservation? (format : AAAA-MM-JJ)")
+    heure = int(input("Pour quelle heure sohautez-vous faire la réservation? (fromat : hh)"))
+    dure = int(input("Donnez la duré de votre réservation. Notez que la réservation doit être d'un minimum d'une. La duré se fait en incrément d'une heure."))
+    reservation = [id_reservation,nom_utilisateur,nom_salle,date,heure,dure]
+    with open("reservations.csv", "w") as fichier_reservation:
+        ecrivain = csv.writer(fichier_reservation)
+        
+        ecrivain.writerow(reservation)
 reserver()
