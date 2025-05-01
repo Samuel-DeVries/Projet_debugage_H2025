15dc21283f85d7ea7ba76ecb0dc669f908a53fed
SanuelDeVries <samuelad2019@outlook.com>
2025-05-01 16:29:27 -0400

    Mise en place du test unitaire

--- Tests/test_fonction_dispo.py
+++ Tests/test_fonction_dispo.py
@@ -2,10 +2,9 @@ import unittest
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Code")))
-from fonction_dispo import *
-print(dispo("2025-04-30", "B1"))
+from fonction_dispo import dispo
 class Test_fonction_dispo(unittest.TestCase):
     def test_dispo_1(self):
         self.assertEqual(type(dispo("2025-04-30", "B1")), dict)
 if __name__ == "__main__":
-    Test_fonction_dispo()
+    unittest.main()
